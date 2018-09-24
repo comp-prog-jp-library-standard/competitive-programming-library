6ad32aff444c05e4b21091ab3d85db3247bbf1e3
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2018-09-25 01:19:13 +0900

    Use clang-format -i, be more informative, run on if format errors are found

--- travis-test.sh
+++ travis-test.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
-set -eux
-# CLANG_FORMAT is the name of clang-format command we want to use
-for src in `find . | grep -E "\.(cpp|h)$"`
+# This script assumes that all files are already staged.
+# It formats all *.cpp, *.h files
+# and check if they become different from the original ones by using git diff.
+# ${CLANG_FORMAT} is the name of clang-format command we want to use
+if [ -z ${CLANG_FORMAT} ]
+then
+    echo 'Please set the variable ${CLANG_FORMAT}.' 1>&2
+    exit 1
+fi
+
+# All *.cpp/*.h files get formatted.
+SOURCE_FILES=`find . | grep -E "\.(cpp|h)$"`
+if ! ${CLANG_FORMAT} -i ${SOURCE_FILES}
+then
+    echo "${CLANG_FORMAT} failed" 1>&2
+    exit 1
+fi
+
+# If git diff finds a difference, print it.
+ERROR_COUNT=0
+for name in `git diff --name-only`
 do
-    diff -u $src <($CLANG_FORMAT $src) ||
-        (echo "Format error: clang-format for file $src" 1>&2; exit 1)
+    echo "File ${name} was ill-formatted and got formatted." 1>&2
+    git diff ${name} 1>&2
+    ERROR_COUNT=`expr ${ERROR_COUNT} + 1`
 done
+
+# Print a summary
+if [ ${ERROR_COUNT} -ne 0 ]
+then
+   echo "Error: ${ERROR_COUNT} file(s)" 1>&2
+   exit 1
+fi
+echo "OK"
