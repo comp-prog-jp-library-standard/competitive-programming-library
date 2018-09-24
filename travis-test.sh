cdf0a6e340627b4e4174c9ce4f61170677522392
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2018-09-24 20:15:43 +0900

    Use env CLANG_FORMAT

--- travis-test.sh
+++ travis-test.sh
@@ -1,9 +1,8 @@
 #/bin/bash
 set -eux
-which clang-format
-which clang-format-6.0
+echo CLANG_FORMAT=$CLANG_FORMAT
 for src in `find . | grep -E "\.(cpp|h)$"`
 do
-    diff -u $src <(clang-format $src) ||
+    diff -u $src <($CLANG_FORMAT $src) ||
         echo "Format error: clang-format for file $src" 1>&2
 done
