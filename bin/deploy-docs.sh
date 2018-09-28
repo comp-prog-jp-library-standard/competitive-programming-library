88c617e04a2070c4b08e6f55a49e97787cb1273b
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2018-09-28 11:59:59 +0900

    Generate JSON file

---
+++ bin/deploy-docs.sh
@@ -0,0 +1,12 @@
+# If not master branch, skip jobs.
+if [ "${CIRCLE_BRANCH}" != "master" ]; then
+  echo "Current branch ${CIRCLE_BRANCH} is not master. Aborted."
+  exit 0
+fi
+
+# Set commiter for auto generated files
+git config user.name "vvataarne"
+git config user.email "is0399ii@ed.ritsumei.ac.jp"
+
+# Generate documents
+python ./bin/generate-docs.py
---
+++ bin/generate-docs.py
@@ -0,0 +1,20 @@
+# This script loads all *.cpp files in ./src and bundle in a single file.
+# It generates JSON and puts into ./docs/lib.json file.
+# It requires Python 3.x (not 2.x)
+
+import glob
+import json
+
+def loadFile(fileName):
+  with open(fileName) as file:
+    return { 'sourceFile': fileName, 'source': file.read() }
+
+# Enumerate files. This includes files in subdirectory.
+fileNames = glob.glob('./src/**.cpp', recursive=True)
+
+# Load file contents.
+fileContents = list(map(loadFile, fileNames))
+
+# Write as a JSON
+with open('docs/lib.json', mode='w') as file:
+  file.write(json.dumps(fileContents))
