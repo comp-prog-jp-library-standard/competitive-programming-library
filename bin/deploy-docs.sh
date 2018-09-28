8765efd87a34bf6a3c124b3c46a27dc2a79fe171
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2018-09-29 08:12:26 +0900

    Consolidate push-docs.sh into deploy-docs.sh

--- bin/deploy-docs.sh
+++ bin/deploy-docs.sh
@@ -11,5 +11,14 @@ git config user.email "is0399ii@ed.ritsumei.ac.jp"
 # Generate documents
 python ./bin/generate-docs.py
 
-# Push changes to GitHub Pages
-./bin/push-docs.sh
+# Get short hash of HEAD (the target commit)
+REVISION=$(git rev-parse --short HEAD)
+
+# Commit changes
+git add docs
+git commit -m "Generate pages for ${REVISION} [ci skip]"
+echo "Commited"
+
+# Push docs to gh-pages branch
+git push origin -f $(git subtree split --prefix docs master):gh-pages
+echo "Pushed"
