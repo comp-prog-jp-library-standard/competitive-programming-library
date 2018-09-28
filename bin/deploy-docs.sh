45a7fc234b052601ae6e83a43e0c0fd43ef493ec
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2018-09-29 08:14:20 +0900

    Filter branches in .circleci/config.yml

--- bin/deploy-docs.sh
+++ bin/deploy-docs.sh
@@ -1,9 +1,3 @@
-# If not master branch, skip jobs.
-if [ "${CIRCLE_BRANCH}" != "master" ]; then
-  echo "Current branch ${CIRCLE_BRANCH} is not master. Aborted."
-  exit 0
-fi
-
 # Set commiter for auto generated files
 git config user.name "vvataarne"
 git config user.email "is0399ii@ed.ritsumei.ac.jp"
