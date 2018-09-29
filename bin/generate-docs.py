03c349e5fb2db13169fea60dd7d16ee886fca0ac
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2018-09-29 18:43:38 +0900

    Fix directory: src -> library/cpp/src

--- bin/generate-docs.py
+++ bin/generate-docs.py
@@ -1,4 +1,4 @@
-# This script loads all *.cpp files in ./src and bundle in a single file.
+# This script loads all *.cpp files in ./library/cpp/src and bundle in a single file.
 # It generates JSON and puts into ./docs/lib.json file.
 # It requires Python 3.x (not 2.x)
 
@@ -10,7 +10,7 @@ def loadFile(fileName):
     return { 'sourceFile': fileName, 'source': file.read() }
 
 # Enumerate files. This includes files in subdirectories.
-fileNames = glob.glob('./src/**.cpp', recursive=True)
+fileNames = glob.glob('./library/cpp/src/**.cpp', recursive=True)
 
 # Load file contents.
 fileContents = list(map(loadFile, fileNames))
