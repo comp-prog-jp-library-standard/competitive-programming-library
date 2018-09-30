e3b3589f4e43c3bd94f6ce9b74987fd2adb3a102
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2018-10-01 01:48:46 +0900

    Add sieve of Eratosthenes

---
+++ library/cpp/src/math/eratosthenes.cpp
@@ -0,0 +1,25 @@
+/*
+ * Sieve of Eratosthenes.
+ * Returns a bool array of length (n + 1)
+ * representing whether integers <= n are prime or not.
+ * if i is a prime, p[i] == true holds.
+ * if i is not a prime, p[i] == false holds.
+ * header requirement: vector
+ */
+namespace procon {
+std::vector<bool> eratosthenes(int n) {
+  std::vector<bool> prime(n + 1);
+  for (int i = 2; i <= n; ++i) {
+    prime[i] = true;
+  }
+  for (int i = 2; i * i <= n; ++i) {
+    if (not prime[i]) {
+      continue;
+    }
+    for (int j = 2 * i; j <= n; j += i) {
+      prime[j] = false;
+    }
+  }
+  return prime;
+}
+} // namespace procon
