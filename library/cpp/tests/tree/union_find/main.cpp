af52c8e47f74209887b429bb25f2325648c0fea8
Akifumi Imanishi <akifumi.imanishi@gmail.com>
2018-10-03 23:10:52 +0900

    fix

--- library/cpp/tests/tree/union_find/main.cpp
+++ library/cpp/tests/tree/union_find/main.cpp
@@ -1,10 +1,11 @@
 // @depends_on library/cpp/src/tree/union_find.cpp
 
 int main(void) {
-  int n, q, com, x, y;
+  int n, q;
   scanf("%d%d", &n, &q);
   procon::UnionFind uf(n);
   while (q--) {
+    int com, x, y;
     scanf("%d%d%d", &com, &x, &y);
     if (com)
       printf("%d\n", uf.is_same_set(x, y));
