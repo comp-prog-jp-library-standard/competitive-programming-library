ca7a0f80ccf1d589c85c66f5be02cc436d58a161
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2018-10-20 22:24:49 +0900

    Add testcase and solution for aoj: 2431 (for range_max_query)

---
+++ library/cpp/tests/array/segment_tree/2431.cpp
@@ -0,0 +1,22 @@
+// @depends_on library/cpp/src/array/segment_tree.cpp
+
+using ll = long long;
+
+int main(void) {
+  int n;
+  scanf("%d", &n);
+  std::vector<int> x(n);
+  for (int i = 0; i < n; ++i)
+    scanf("%d", &x[i]);
+  auto seg =
+      procon::make_range_max_query(n + 1, std::numeric_limits<ll>::min());
+  ll ma = 0;
+  for (int i = 0; i < n; ++i) {
+    ll res = std::max(seg.query(1, x[i]), 0LL);
+    ll cur = res + x[i];
+    ma = std::max(ma, cur);
+    seg.update(x[i], cur);
+  }
+  ll whole = (ll)n * (ll)(n + 1) / 2;
+  printf("%lld\n", whole - ma);
+}
